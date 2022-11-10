4916bbba120f51484f04cf2958e5e5494d1ea0fc
Azazelo967 <al.shmalts@yandex.ru>
2022-11-10 13:58:50 +0600

    Hh parser is ready. Added file for parsing superjob vacancies

--- hh_parser.py
+++ hh_parser.py
@@ -13,19 +13,33 @@ headers = {
 def hh_get_pages(first_url: str):
     hh_request = requests.get(first_url, headers=headers)  # get HTML of the first page
     hh_soup = BeautifulSoup(hh_request.text, features="html.parser")  # define of seeker
-    # print(hh_request.html.html)
     paginator = hh_soup.find("div", {'class': "pager", 'data-qa': "pager-block"})  # get block with the page urls
     last_page = int(paginator.find_all('a')[-2].text)  # the last page
     list_pages = []
     for page in range(last_page//10):
-        list_pages.append(requests.get(first_url+f"&page={page}", headers=headers))
+        list_pages.append(requests.get(first_url+f"&page={page}", headers=headers, timeout=15))
     return list_pages  # return list of all pages
 
 
-def hh_get_vacancies(hh_response: requests.models.Response):
-    list_vacancies = []
-    page = BeautifulSoup(hh_response.text, 'html.parser').find_all("span", {'data-page-analytics-event': "vacancy_search_suitable_item"})
-    for vacancy in page:
-        list_vacancies.append(vacancy.text)
-    return list_vacancies
+def hh_get_title(hh_item: BeautifulSoup):
+    return hh_item.find("span", {'data-page-analytics-event': "vacancy_search_suitable_item"}).find("a").text
 
+
+def hh_get_company(hh_item: BeautifulSoup):
+    return hh_item.find("div", {'class': "vacancy-serp-item__meta-info-company"}).find("a").text
+
+
+def hh_get_location(hh_item: BeautifulSoup):
+    return hh_item.find("div", {'data-qa': "vacancy-serp__vacancy-address"}).text.partition(',')[0]
+
+
+def hh_get_link(hh_item: BeautifulSoup):
+    return hh_item.find("span", {'data-page-analytics-event': "vacancy_search_suitable_item"}).find('a')['href']
+
+
+def hh_get_vacancies(hh_page: requests.models.Response):
+    hh_items = BeautifulSoup(hh_page.text, features='html.parser').find_all("div", {'class': "vacancy-serp-item__layout"})
+    vacancies = []
+    for vacancy in hh_items:
+        vacancies.append({'title': hh_get_title(vacancy), 'company': hh_get_company(vacancy), 'location': hh_get_location(vacancy), 'link': hh_get_link(vacancy)})
+    return vacancies
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from hh_parser import hh_get_pages, hh_get_vacancies
 
-hh_pages = hh_get_pages("https://hh.ru/search/vacancy?text=python&order_by=relevance&items_on_page=20")
-
-for page in hh_pages:
-    a = hh_get_vacancies(page)
-    print(a)
-
-
-
+# hh_pages = hh_get_pages("https://hh.ru/search/vacancy?text=python&order_by=relevance&items_on_page=20")
+# hh_vacancies = []
+# for page in hh_pages:
+#     hh_vacancies.extend(hh_get_vacancies(page))
+#
+# print(*hh_vacancies, sep='\n')

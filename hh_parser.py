d54e8ad0ca1de27b8da5b81c6c3d07c060b881a4
Azazelo967 <al.shmalts@yandex.ru>
2022-11-15 12:39:38 +0600

    Changed method of getting hh_vacancies, preparing /report web page

--- hh_parser.py
+++ hh_parser.py
@@ -10,13 +10,14 @@ headers = {
 }  # headers for requests
 
 
-def hh_get_pages(first_url: str):
+def hh_get_pages(key_word: str):
+    first_url = f"https://hh.ru/search/vacancy?text={key_word}&order_by=relevance&items_on_page=20"
     hh_request = requests.get(first_url, headers=headers)  # get HTML of the first page
     hh_soup = BeautifulSoup(hh_request.text, features="html.parser")  # define of seeker
     paginator = hh_soup.find("div", {'class': "pager", 'data-qa': "pager-block"})  # get block with the page urls
     last_page = int(paginator.find_all('a')[-2].text)  # the last page
     list_pages = []
-    for page in range(last_page//10):
+    for page in range(last_page//10) if range(last_page//10) != 0 else 1:  # take not all pages
         list_pages.append(requests.get(first_url+f"&page={page}", headers=headers, timeout=15))
     return list_pages  # return list of all pages
 
@@ -37,9 +38,11 @@ def hh_get_link(hh_item: BeautifulSoup):
     return hh_item.find("span", {'data-page-analytics-event': "vacancy_search_suitable_item"}).find('a')['href']
 
 
-def hh_get_vacancies(hh_page: requests.models.Response):
-    hh_items = BeautifulSoup(hh_page.text, features='html.parser').find_all("div", {'class': "vacancy-serp-item__layout"})
+def hh_get_vacancies(key_word: str):
+    hh_pages = hh_get_pages(key_word)
     vacancies = []
-    for vacancy in hh_items:
-        vacancies.append({'title': hh_get_title(vacancy), 'company': hh_get_company(vacancy), 'location': hh_get_location(vacancy), 'link': hh_get_link(vacancy)})
+    for hh_page in hh_pages:
+        hh_items = BeautifulSoup(hh_page.text, features='html.parser').find_all("div", {'class': "vacancy-serp-item__layout"})
+        for vacancy in hh_items:
+            vacancies.append({'title': hh_get_title(vacancy), 'company': hh_get_company(vacancy), 'location': hh_get_location(vacancy), 'link': hh_get_link(vacancy)})
     return vacancies
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask("JobScrapper")
 
@@ -10,7 +10,12 @@ def home():
 
 @app.route('/report')
 def report():
-    return render_template('report.html', job=request.args.get('job'))
+    job = request.args.get('job')
+    if job is None or not str(job):
+        return redirect('/')
+    else:
+        job.lower()
+    return render_template('report.html', job=job)
 
 
 app.run(host="0.0.0.0")
@@ -19,19 +24,16 @@ app.run(host="0.0.0.0")
 # from sj_parser import sj_get_pages, sj_get_vacancies
 # import csv
 #
-# hh_pages = hh_get_pages("https://hh.ru/search/vacancy?text=python&order_by=relevance&items_on_page=20")
-# hh_vacancies = []
-# for page in hh_pages:
-#     hh_vacancies.extend(hh_get_vacancies(page))
 #
+# # sj_pages = sj_get_pages("https://russia.superjob.ru/vacancy/search/?keywords=python&click_from=facet")
+# # sj_vacancies = []
+# # for page in sj_pages:
+# #     sj_vacancies.extend(sj_get_vacancies(page))
 #
-# sj_pages = sj_get_pages("https://russia.superjob.ru/vacancy/search/?keywords=python&click_from=facet")
-# sj_vacancies = []
-# for page in sj_pages:
-#     sj_vacancies.extend(sj_get_vacancies(page))
+# hh_vacancies = hh_get_vacancies("huawei")
 #
-# all_vacancies = sj_vacancies + hh_vacancies
-# writer = csv.writer(open("test.csv", mode='w'))
+# all_vacancies = hh_vacancies
+# writer = csv.writer(open("test.csv", mode='w', encoding="UTF-8"))
 # writer.writerow([*all_vacancies[0].keys()])
 # for vacancy in all_vacancies:
 #     writer.writerow([*vacancy.values()])
